5634b5229393f9c60fcfa49e483b752faa655b03
GaneshGotame <83527532+GaneshGotame@users.noreply.github.com>
2021-06-02 17:58:30 +0545

    Add files via upload

---
+++ shooting.cpp
@@ -0,0 +1,24 @@
+#include<graphics.h>
+#include<conio.h>
+
+int main()
+{	
+	
+	initwindow(2000,1000,"shoot");
+	setbkcolor(WHITE);
+	cleardevice();
+//	readimagefile("girl.jpg",200,300,450,600);
+	readimagefile("mix1.jpg",200,200,800,600);
+	if(getch()=='u')
+//	for(int i=0;i<500;i+=10)
+	 {
+	 //	readimagefile("bullet3.jfif",415+i,350,425+i,355);	
+	 
+	 }
+	
+	getch();
+	closegraph();
+	
+	
+	
+}
